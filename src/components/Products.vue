2bd436f97fdb628107014be22c6b463b4c26fdfb
Bryan Gordon <bryanbge92@gmail.com>
2025-02-13 23:50:52 -0500

    Create component

---
+++ src/components/Products.vue
@@ -0,0 +1,50 @@
+<script setup>
+  import ProductsList from '../store/products.json'
+
+  const products = ProductsList 
+</script>
+
+<template>
+  <section>
+    <header>
+      <h3>Lista de postres</h3>
+    </header>
+
+    <article v-for="product in products">
+      <picture>
+        <img :src=product.image :alt=product.name_product>
+      </picture>
+
+      <div>
+        <h4>{{ product.name_product }}</h4>
+        <p>{{ product.product_description }}</p>
+      </div>
+    </article>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    width: 75%;
+    color: wheat;
+    margin: 0 auto;
+  }
+
+  h3 {
+    font-size: 24px;
+    margin: 24px 0 30px 0;
+  }
+
+  article {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  img {
+    max-width: 200px;
+  }
+
+  div {
+    max-width: 75%;
+  }
+</style>

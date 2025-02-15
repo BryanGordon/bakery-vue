e2961f6b1d0281edd77a5276181246e33e1b90b0
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:51:39 -0500

    Create Instagram icon

---
+++ src/icons/Instagram.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  <a target="_blank" ref="noreferrer" href="https://www.instagram.com">
+    <svg  
+      width="40"  
+      height="40"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="whiteSmoke"  
+      stroke-width="1.5"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+      <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+      <path d="M16.5 7.5v.01" />
+    </svg>
+  </a>
+</template>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+
+  svg {
+    margin-inline: 20px;
+  }
+
+  svg:hover {
+    stroke: #ff6a81;
+    transition: ease 0.3s;
+  }
+</style>

d07b573c4212a4af3f32f8aae44c815af987d8bf
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:51:25 -0500

    Create Facebook icon

---
+++ src/icons/Facebook.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <a target="_blank" ref="noreferer" href="https://www.facebook.com">
+    <svg  
+      width="40"  
+      height="50"  
+      viewBox="0 0 24 24"  
+      fill="whiteSmoke"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M18 2a1 1 0 0 1 .993 .883l.007 .117v4a1 1 0 0 1 -.883 .993l-.117 .007h-3v1h3a1 1 0 0 1 .991 1.131l-.02 .112l-1 4a1 1 0 0 1 -.858 .75l-.113 .007h-2v6a1 1 0 0 1 -.883 .993l-.117 .007h-4a1 1 0 0 1 -.993 -.883l-.007 -.117v-6h-2a1 1 0 0 1 -.993 -.883l-.007 -.117v-4a1 1 0 0 1 .883 -.993l.117 -.007h2v-1a6 6 0 0 1 5.775 -5.996l.225 -.004h3z" />
+    </svg>
+  </a>
+</template>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+
+  svg:hover {
+    fill: #ff6a81;
+    transition: ease 0.3s;
+  }
+</style>

2461ee570e989f1d67f65a62145a22fcae7d7232
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:50:11 -0500

    Update styles

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -6,24 +6,30 @@
   <footer>
     <div>
       <span class="span-footer">Copyright</span>
-      <a href="#">Inicio</a>
+      <a href="#inicio">Inicio</a>
     </div>
   </footer>
 </template>
 
 <style scoped>
   footer {
-    background-color: aquamarine;
+    background-color: #0D0D0D;
     padding-block: 15px;
     margin-top: auto;
   }
 
   div {
-    margin-inline: 20px;
+    margin-inline: 35px;
   }
 
   a {
     float: right;
     text-decoration: none;
+    color: whitesmoke;
+  }
+
+  a:hover {
+    color: #ff6a81;
+    transition: ease 0.3s;
   }
 </style>

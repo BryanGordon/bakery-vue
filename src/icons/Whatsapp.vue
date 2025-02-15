5c6ab6334f431c52f5c10b180b933e750c0aebb6
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:51:52 -0500

    Create Whatsapp

---
+++ src/icons/Whatsapp.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <a target="_blank" ref="noreferrer" href="https://www.whatsapp.com">
+    <svg  
+      width="40"  
+      height="40"  
+      viewBox="0 0 24 24"  
+      fill="none"  
+      stroke="whiteSmoke"  
+      stroke-width="1.5"  
+      stroke-linecap="round"  
+      stroke-linejoin="round"  
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+      <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+    </svg>
+</a>
+</template>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+
+  svg:hover {
+    stroke: #ff6a81;
+    transition: ease 0.3s;
+  }
+</style>

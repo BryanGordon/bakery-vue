f03e171e5ae4e3f6953ad206a5e20eb06f7750c1
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:51:10 -0500

    Update component
    - Add styles
    - Change structure

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -10,14 +10,18 @@
       <h3>Lista de postres</h3>
     </header>
 
-    <article v-for="product in products">
-      <picture>
-        <img :src=product.image :alt=product.name_product>
-      </picture>
+    <article>
+      <div class="container-products" v-for="product in products">
+        
+        <picture>
+          <img :src=product.image :alt=product.name_product>
+        </picture>
 
-      <div>
-        <h4>{{ product.name_product }}</h4>
-        <p>{{ product.product_description }}</p>
+        <div class="product-info-container">
+          <h4>{{ product.name_product }}</h4>
+          <p>{{ product.product_description }}</p>
+        </div>
+
       </div>
     </article>
   </section>
@@ -26,7 +30,6 @@
 <style scoped>
   section {
     width: 75%;
-    color: wheat;
     margin: 0 auto;
   }
 
@@ -35,16 +38,31 @@
     margin: 24px 0 30px 0;
   }
 
-  article {
+  .container-products {
     display: flex;
     justify-content: space-evenly;
+    margin-bottom: 50px;
   }
 
   img {
     max-width: 200px;
+    height: 150px;
+    border-radius: 20px;
   }
 
-  div {
+  .product-info-container {
     max-width: 75%;
+    margin-left: 30px;
   }
+
+  @media screen and (width <= 950px) {
+    .container-products {
+      flex-direction: column;
+    }
+
+    .product-info-container {
+      margin: 0;
+    }
+  }
+
 </style>

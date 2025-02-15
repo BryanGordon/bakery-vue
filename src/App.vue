5673c4a2b620a4561d9f4dd82db7cbb399c3a064
Bryan Gordon <bryanbge92@gmail.com>
2025-02-14 19:49:43 -0500

    Update app structure

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   import HeaderApp from './components/HeaderApp.vue';
   import FooterApp from './components/FooterApp.vue';
   import Products from './components/Products.vue';
+  import Contact from './components/Contact.vue';
 </script>
 
 <template>
   <HeaderApp />
   <Products />
+  <Contact />
   <FooterApp />
 </template>
